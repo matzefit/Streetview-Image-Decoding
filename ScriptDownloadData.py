f9dfcd69caded3e6f9615366642cd7a08efad6c3
unknown <matthias.fitzky@gmail.com>
2023-10-29 20:01:51 -0400

    updated Download Data Script with vehicle data

--- ScriptDownloadData.py
+++ ScriptDownloadData.py
@@ -8,5 +8,5 @@ downloader.get_available_packages(session=session)
 #data choice
 print('downloading gtfine and leftImg8bit packages ... \n')
 
-package_list = {'gtFine_trainvaltest.zip', 'leftImg8bit_trainvaltest.zip'}
-downloader.download_packages(session=session, package_names=package_list, destination_path=r'C:\Users\Citylab\Documents\PythonProjects\ComputerVision\SemesterProject\Data')
+package_list = {'gtFine_trainvaltest.zip', 'leftImg8bit_trainvaltest.zip','vehicle_trainvaltest.zip'}
+downloader.download_packages(session=session, package_names=package_list, destination_path=r'\Data')
